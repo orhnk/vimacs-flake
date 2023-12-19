301c43fa81032ceecbb0eceb68398f71c6d3c513
utfeight <utfeightt@gmail.com>
2023-12-19 22:46:32 +0300

    build: update flake

--- flake.nix
+++ flake.nix
@@ -1,57 +1,67 @@
 {
-  description = "NvChad's Neovim Configuration";
+  description = "IogaMaster's Neovim Configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-    ...
-  }:
-    utils.lib.eachDefaultSystem (
-      system: let
-        inherit (nixpkgs) lib;
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
+    let
+      inherit (nixpkgs) lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+      runtimeDeps = with pkgs; [ 
+        # Tree sitter
+        gcc 
+        
+        # LSP/Linters
+        nil 
+        statix 
+        deadnix 
+        manix
 
-        nvim =
-          pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped
-          (pkgs.neovimUtils.makeNeovimConfig
-            {
-              customRC = ''
-                set runtimepath^=${./.}
-                source ${./.}/init.lua
-              '';
-            }
-            // {
-              wrapperArgs = [
-                "--prefix"
-                "PATH"
-                ":"
-                "${lib.makeBinPath [pkgs.gcc pkgs.nil pkgs.chafa pkgs.ffmpegthumbnailer pkgs.wakatime pkgs.statix]}"
-              ];
-            });
-      in {
-        overlays = {
-          neovim = _: _prev: {
-            neovim = nvim;
-          };
-          default = self.overlays.neovim;
-        };
+        # Telescope
+        chafa 
+        ffmpegthumbnailer 
 
-        packages = rec {
-          neovim = nvim;
-          default = neovim;
-        };
+        # Misc
+        wakatime 
+      ];
 
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [
-            pkgs.stylua
+      nvim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped
+        (pkgs.neovimUtils.makeNeovimConfig
+          {
+            customRC = ''
+              set runtimepath^=${./.}
+              source ${./.}/init.lua
+            '';
+          } // {
+          wrapperArgs = [
+            "--prefix"
+            "PATH"
+            ":"
+            "${lib.makeBinPath runtimeDeps}"
           ];
+        });
+    in
+    {
+      overlays = {
+        neovim = _: _prev: {
+          neovim = nvim;
         };
-      }
-    );
+        default = self.overlays.neovim;
+      };
+
+      packages = rec {
+        neovim = nvim;
+        default = neovim;
+      };
+
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [
+          pkgs.stylua
+        ];
+      };
+    }
+  );
 }
